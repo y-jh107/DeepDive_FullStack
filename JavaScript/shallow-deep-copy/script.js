07e836908b9ce416007bf64b414c4af424e9ab53
y-jh107 <jin99545@naver.com>
2026-01-05 15:28:14 +0900

    Shallow, Deep Copy

---
+++ JavaScript/shallow-deep-copy/script.js
@@ -0,0 +1,62 @@
+// // shallow copy
+// const aArray = [1, 2, 3];
+
+// // shallow copy with spread operator
+// const bArray = [...aArray, 4];
+
+// console.log('aArray', aArray);
+// console.log('bArray', bArray);
+// console.log('aArray === bArray', aArray === bArray);
+
+// // shallow copy with Object.assign()
+
+// const cArray = Object.assign([], bArray);
+
+// console.log('cArray', cArray);
+// console.log('bArray === cArray', bArray === cArray);
+
+// // Nested Arrays and Objects
+// cArray.push([5, 6, 7]);
+// console.log('cArray', cArray);
+
+// const dArray = [...cArray, 10];
+// console.log('dArray', dArray);
+
+// dArray[4].push(8);
+// console.log('cArray', cArray);
+// console.log('dArray', dArray); // 중첩된 부분은 변경됨
+
+// 얕은 동결
+
+// Object.freeze(aObject);
+// aObject.a = 'c';
+// console.log('aObject', aObject);
+
+// aObject.cObject.a = 3;
+// console.log('aObject', aObject);
+
+const aObject = {
+    'a': 'a',
+    'b': 'b',
+    'cObject': {'a': 1, 'b': 2}
+}
+
+// deep copy with json.parse(json.stringify())
+// const newAObject = JSON.parse(JSON.stringify(aObject));
+
+// console.log('aObject', aObject);
+// console.log('newAObject', newAObject);
+// aObject.cObject.a = 3;
+// console.log('aObject', aObject);
+// console.log('newAObject', newAObject);
+
+// deep copy with nested spread operator
+const newAObject = {...aObject, cObject: {...aObject.cObject}};
+
+console.log('aObject', aObject);
+console.log('newAObject', newAObject);
+aObject.cObject.a = 3;
+console.log('aObject', aObject);
+console.log('newAObject', newAObject);
+
+// 이외에도 lodash 라이브러리 또는 structuredClone() 사용
