7e5b724564f076859096c055661e829edee7d252
y-jh107 <jin99545@naver.com>
2026-01-06 11:13:53 +0900

    Condition Loop

---
+++ Goorm_EXP/JavaScript_기초/Condition-Loops/script.js
@@ -0,0 +1,17 @@
+const numArray = [2, 7, 13, 29, 13, 16, 8, 36, 19];
+
+console.log(numArray);
+
+for (let i = 0; i < numArray.length; i++) {
+    if (numArray[i] % 2 === 0) {
+        console.log(`${i}번째 숫자는 짝수입니다.(${numArray[i]})`);
+    }
+}
+
+const filterArray = [3, 25, 19, 12, 48, 92, 10];
+
+for (number of filterArray) {
+    if (number % 3 === 0) {
+        console.log(`${number}는 3의 배수입니다.`);
+    }
+}
