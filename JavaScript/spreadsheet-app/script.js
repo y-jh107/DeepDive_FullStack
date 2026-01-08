2e902b68f72c0ab080a0353db067dbb0b10b7d02
y-jh107 <jin99545@naver.com>
2026-01-08 12:06:22 +0900

    Header Style, RowCol Selecting

--- JavaScript/spreadsheet-app/script.js
+++ JavaScript/spreadsheet-app/script.js
@@ -32,12 +32,14 @@ const alphabets = [
 ]
 
 class Cell {
-    constructor(isHeader, disabled, data, row, column, active = false) {
+    constructor(isHeader, disabled, data, row, column, rowName, columnName, active = false) {
         this.isHeader = isHeader;
         this.disabled = disabled;
         this.data = data;
         this.row = row;
         this.column = column;
+        this.rowName = rowName;
+        this.columnName = columnName;
         this.active = active;
     }
 }
@@ -49,14 +51,20 @@ function initSpreadsheet() {
         let spreadsheetRow = [];
         for (let j = 0; j < COLS; j++) {
             let cellData = '';
+            let isHeader = false;
+            let disabled = false;
 
             // 모든 row 첫 번째 column에 숫자 넣기
             if (j === 0) {
                 cellData = i;
+                isHeader = true;
+                disabled = true;
             }
 
             if (i === 0) {
                 cellData = alphabets[j - 1];
+                isHeader = true;
+                disabled = true;
             }
 
             // 첫 번째 row의 column은 ""
@@ -64,7 +72,10 @@ function initSpreadsheet() {
                 cellData = "";
             }
 
-            const cell = new Cell(false, false, cellData, i, j, false);
+            const rowName = i;
+            const columnName = alphabets[j - 1];
+
+            const cell = new Cell(isHeader, disabled, cellData, i, j, rowName, columnName, false);
 
             spreadsheetRow.push(cell);
         }
@@ -82,7 +93,29 @@ function createCellEl(cell) {
     cellEl.value = cell.data;
     cellEl.disabled = cell.disabled;
 
+    if (cell.isHeader) {
+        cellEl.classList.add('header');
+    }
+
+    cellEl.onclick = () => handleCellClick(cell);
+
     return cellEl;
+}
+
+function handleCellClick(cell) {
+    const columnHeader = spreadsheet[0][cell.column];
+    const rowHeader = spreadsheet[cell.row][0];
+    const columnHeaderEl = getElFromRowCol(columnHeader.row, columnHeader.column);
+    const rowHeaderEl = getElFromRowCol(rowHeader.row, rowHeader.column);
+
+    columnHeaderEl.classList.add('active');
+    rowHeaderEl.classList.add('active');
+    
+    console.log('clicked cell', columnHeaderEl, rowHeaderEl);
+}
+
+function getElFromRowCol(row, col) {
+    return document.querySelector('#cell_' + row + col);
 }
 
 function drawSheet() {
