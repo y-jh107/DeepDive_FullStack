605bf925748db59aa7b56c669613c8fd9a59e20b
y-jh107 <jin99545@naver.com>
2026-01-14 14:04:10 +0900

    연산자

--- JavaScript2/section3/src/index.js
+++ JavaScript2/section3/src/index.js
@@ -54,9 +54,44 @@ let name5;
 
 console.log(name5);
 
-let num1 = "15";
-let num2 = 5;
+// let num1 = 10;
+// let num2 = 5;
 
-console.log(num1 / num2); // 묵시적 형변환
-console.log(num1 + num2);
-console.log(parseInt(num1) + num2); // 명시적 형변환
+// console.log(num1 / num2); // 묵시적 형변환
+// console.log(num1 + num2);
+// console.log(parseInt(num1) + num2); // 명시적 형변환
+
+// console.log(num1 + num2);
+// console.log(num1 - num2);
+// console.log(num1 * num2);
+// console.log(num1 / num2);
+
+// console.log(num1 % 2);
+// console.log(num2 % 2);
+
+// let num = 10;
+
+// console.log(num++); // 후위연산 -> 10
+// console.log(num); // 현재 num 값 -> 11
+// console.log(++num); // 전위연산 -> 12
+
+// let num2 = 10;
+
+// console.log(num2--); // 후위연산 -> 10
+// console.log(num2); // 현재 num 값 -> 9
+// console.log(--num2); // 전위연산 -> 8
+
+let num1 = 10;
+let num2 = "10";
+
+console.log(num1 === num2); // type 검사
+console.log(num1 == num2); // 값만 검사
+
+let num;
+num = num ?? 20; // null병합 연산자, undefined나 null이면 20 대입
+
+console.log(num);
+
+let num3 = 100;
+
+console.log(num3 % 2 === 0 ? "짝수" : "홀수");
