cd0068cec309792634689dc37368405e9e98dde1
y-jh107 <jin99545@naver.com>
2026-01-14 13:31:58 +0900

    변수와 상수

--- JavaScript2/section3/src/index.js
+++ JavaScript2/section3/src/index.js
@@ -1,1 +1,16 @@
-console.log("start VSCode");
+// let color = "skyblue";
+// color = "yellow";
+// console.log(color);
+
+let isCatOrDog; // 카멜 표기법(첫 단어를 제외한 나머지 단어의 시작을 대문자로)
+
+let nowType = "안녕하세요";
+
+console.log(typeof nowType);
+
+nowType = 100;
+
+console.log(typeof nowType);
+
+const color = "skyblue";
+color = "yellow";
