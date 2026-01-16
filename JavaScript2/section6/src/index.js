6c62b01433672d3ffc6025558c984a3dd40b6eb9
y-jh107 <jin99545@naver.com>
2026-01-16 10:50:50 +0900

    Date 객체

---
+++ JavaScript2/section6/src/index.js
@@ -0,0 +1,13 @@
+// let nowDate = new Date();
+// console.log(nowDate);
+
+// const week = ["일", "월", "화", "수", "목", "금", "토"];
+
+// let month = nowDate.getMonth() + 1;
+// let date = nowDate.getDate();
+// let day = nowDate.getDay();
+// let hours = nowDate.getHours(); // 0-24
+// let minutes = nowDate.getMinutes(); // 0-59
+
+// console.log(`${month}월 ${date}일 ${week[day]}요일`);
+// console.log(`${hours}:${minutes}`);
