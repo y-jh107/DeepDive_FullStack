7e0732e9f8ef2bf1af16ca8325592edb0f2ca73a
y-jh107 <jin99545@naver.com>
2026-01-28 12:06:16 +0900

    Context 사용하기

--- React2/react-practice-day03/src/components/PostItem.jsx
+++ React2/react-practice-day03/src/components/PostItem.jsx
@@ -1,20 +1,29 @@
 import "./PostItem.css";
-import { memo } from "react";
+import { memo, useRef } from "react";
 
 const PostItem = ({ id, title, isPublished, onUpdate, onDelete }) => {
+  const releaseRef = useRef();
+
   const onClickDeleteBtn = () => {
     onDelete(id);
   };
 
   const onClickReleaseBtn = () => {
     onUpdate(id);
+    if (!isPublished) {
+      releaseRef.current.classList.add("not-release");
+      releaseRef.current.classList.remove("release");
+    } else {
+      releaseRef.current.classList.add("release");
+      releaseRef.current.classList.remove("not-release");
+    }
   };
 
   return (
     <div className="PostItem">
       <div>📌</div>
       <div className="content">{title}</div>
-      <button onClick={onClickReleaseBtn} className="release">
+      <button ref={releaseRef} onClick={onClickReleaseBtn} className="release">
         {isPublished ? "비공개" : "공개"}
       </button>
       <button onClick={onClickDeleteBtn} className="delete">
---
+++ React2/section11/src/components/TodoItem.jsx
@@ -0,0 +1,45 @@
+import "./TodoItem.css";
+import { memo, useRef, useContext } from "react";
+import { TodoDispatchContext } from "../App";
+
+const TodoItem = ({ id, isDone, content, date }) => {
+  const { onUpdate, onDelete } = useContext(TodoDispatchContext);
+  const contentRef = useRef();
+
+  const onChangeCheckbox = () => {
+    onUpdate(id);
+    if (!isDone) contentRef.current.classList.add("clear");
+    else contentRef.current.classList.remove("clear");
+  };
+
+  const onClickDeleteButton = () => {
+    onDelete(id);
+  };
+
+  return (
+    <div className="TodoItem">
+      <input onChange={onChangeCheckbox} checked={isDone} type="checkbox" />
+      <div ref={contentRef} className="content">
+        {content}
+      </div>
+      <div className="date">{new Date(date).toLocaleDateString()}</div>
+      <button onClick={onClickDeleteButton}>삭제</button>
+    </div>
+  );
+};
+
+// 고차 컴포넌트 (HOC, Higher Order Component)
+// export default memo(TodoItem, (prevProps, nextProps) => {
+//   // 반환값에 따라 Props가 바뀌었는지 안바뀌었는지 판단
+//   // T -> Props 바뀌지 않음 -> 리렌더링 X
+//   // F -> Props 바뀜 -> 리렌더링 O
+
+//   if (prevProps.id !== nextProps.id) return false;
+//   if (prevProps.isDone !== nextProps.isDone) return false;
+//   if (prevProps.content !== nextProps.content) return false;
+//   if (prevProps.date !== nextProps.date) return false;
+
+//   return true;
+// });
+
+export default memo(TodoItem);
