676a5cca50ae47414988f5336613af69308d79aa
y-jh107 <jin99545@naver.com>
2026-01-28 16:54:42 +0900

    Enum 타입

--- TypeScript/section02/dist/chapter4.js
+++ TypeScript/section02/dist/chapter4.js
@@ -1,18 +1,27 @@
-// 타입 별칭
 let user = {
-  id: 1,
-  name: "양진혁",
-  nickname: "memm",
-  birth: "1999.01.07",
-  bio: "hello",
-  location: "성남시",
+    id: 1,
+    name: "양진혁",
+    nickname: "memm",
+    birth: "1999.01.07",
+    bio: "hello",
+    location: "성남시",
 };
 let user2 = {
-  id: 2,
-  name: "홍길동",
-  nickname: "memm",
-  birth: "1999.01.07",
-  bio: "hello",
-  location: "성남시",
+    id: 2,
+    name: "홍길동",
+    nickname: "memm",
+    birth: "1999.01.07",
+    bio: "hello",
+    location: "성남시",
+};
+let countryCodes = {
+    Korea: "ko",
+    UnitedStates: "us",
+    UnitedKingdom: "uk",
+};
+let countryNumberCodes = {
+    Korea: 410,
+    UnitedState: 840,
+    UnitedKingdom: 826,
 };
 export {};
